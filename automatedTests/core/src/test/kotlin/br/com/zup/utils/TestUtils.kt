1619ff97ea808ebe3fc874620704d54a9ccb1652
Lucas Sousa Silva <lucas.silva@zup.com.br>
2020-08-27 17:08:56 -0300

    verifing if webdriver is already configured

--- automatedTests/core/src/test/kotlin/br/com/zup/utils/TestUtils.kt
+++ automatedTests/core/src/test/kotlin/br/com/zup/utils/TestUtils.kt
@@ -3,28 +3,40 @@ package br.com.zup.utils
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
+import java.lang.Exception
 import java.util.concurrent.TimeUnit
 
 object TestUtils {
 
+    val options = ChromeOptions().apply {
+        addArguments(
+                "--no-sandbox",
+                "--disable-dev-shm-usage",
+                "--headless"
+        )
+    }
+
+    private fun isDriverSet(): Boolean {
+        try {
+            ChromeDriver(options)
+            return true
+        } catch (exception: Exception) {}
+        return false
+    }
+
     fun createDriver(url: String): WebDriver {
         val driverKey = UtilResources.getProperties("nameDriver")
         println("TESTE!!!!!!!!")
-        if (System.getProperty(driverKey)?.isEmpty() != false) {
+        if (!isDriverSet()) {
+            println("NOT SET")
             System.setProperty(
                     driverKey,
                     UtilResources.getProperties("pathDriver") +
                             UtilResources.getProperties("exeDriver"))
         } else {
-            println(System.getProperty(driverKey))
+            println("SET")
         }
 
-        val options = ChromeOptions()
-        options.addArguments(
-                "--no-sandbox",
-                "--disable-dev-shm-usage",
-                "--headless"
-        )
         val driver = ChromeDriver(options)
         driver.manage()?.timeouts()?.implicitlyWait(10, TimeUnit.SECONDS)
         driver.manage()?.window()?.maximize()
