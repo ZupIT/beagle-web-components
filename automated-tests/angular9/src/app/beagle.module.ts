dbf52d36b261ba36169415f058d1d6a22f2a510d
tayronearaujo <tayron_machado@hotmail.com>
2020-12-22 17:34:28 -0300

    added analytics config

--- automated-tests/angular9/src/app/beagle.module.ts
+++ automated-tests/angular9/src/app/beagle.module.ts
@@ -1,6 +1,14 @@
 import { BeagleModule } from '@zup-it/beagle-angular';
 import { environment } from './../environments/environment';
+import { AnalyticsConfig } from '@zup-it/beagle-web'
 // import all the components you wish to use with Beagle.
+
+const analyticsConfig: AnalyticsConfig = {
+  enableScreenAnalytics: true,
+  actions: {
+    'beagle:alert': ['message']
+  }
+}
 
 @BeagleModule({
   baseUrl: environment.baseUrl || 'http://localhost:4200/public',
@@ -11,5 +19,17 @@ import { environment } from './../environments/environment';
   components: {
     // Associate every beagle component to your angular component.
   },
+  analyticsProvider: {
+    createRecord: (record) => {
+      console.log('record', record)
+    },
+    getConfig: () => new Promise(resolve => {
+      return resolve(analyticsConfig)
+    }),
+    startSession: () => new Promise(resolve => {
+      return resolve()
+    })
+  }
+  
 })
 export class Beagle {}
