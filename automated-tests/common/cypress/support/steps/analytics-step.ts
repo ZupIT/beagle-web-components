1ea4f4ceea184e861a046c980e1d1950426eaeda
tayronearaujo <tayron_machado@hotmail.com>
2020-12-23 11:18:07 -0300

    added initial tests

---
+++ automated-tests/common/cypress/support/steps/analytics-step.ts
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import AnalyticsPage from '../page-objects/AnalyticsPage'
+
+const analyticsPage = new AnalyticsPage
+
+Given("the Beagle application did launch with the Analytics screen url", () => {
+    analyticsPage.init()
+})
+
+When(/I press the button with title "(.*)"/, (buttonText) => {
+    analyticsPage.clickButtonByText(buttonText)
+})
+
+// Then("an alert dialog should appear on the screen", () => {
+// })
+
+// Then("a confirm dialog should appear on the screen", () => {
+// })
+
+Then("no analytics record should be created", () => {
+
+})
