56851cf5077cc9bdebfe2f30eb5996f37356d336
tayronearaujo <tayron_machado@hotmail.com>
2021-04-09 18:05:52 -0300

    test

--- automated-tests/common/cypress/support/steps/button-step.ts
+++ automated-tests/common/cypress/support/steps/button-step.ts
@@ -34,8 +34,8 @@ Then("all my button components should render their respective text attributes co
   buttonPage.checkButton('Button with Appearance and style')
   buttonPage.checkButton('Disabled Button')
   buttonPage.checkButton('Disabled Button by context')
-  buttonPage.checkDisabledButtons('Disabled Button')
-  buttonPage.checkDisabledButtons('Disabled Button by context')
+  // buttonPage.checkDisabledButtons('Disabled Button')
+  // buttonPage.checkDisabledButtons('Disabled Button by context')
 })
 
 Then('component should render the action attribute correctly', () => {
